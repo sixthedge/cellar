c107ac4ebafa2b42d68069e69254d4c1703a2a35
Zachary Johnson <rootwizzy@gmail.com>
2017-03-30 16:53:32 -0500

    [FIX] Add engine property to route helpers

--- src/thinkspace/api/thinkspace-readiness-assurance/test/readiness_assurance/helpers/readiness_assurance_helper.rb
+++ src/thinkspace/api/thinkspace-readiness-assurance/test/readiness_assurance/helpers/readiness_assurance_helper.rb
@@ -1,6 +1,6 @@
 require_relative '../../../../thinkspace-casespace/test/helpers/controller_helper'
 require_relative '../../../../thinkspace-casespace/test/phase_actions/helpers/phase_actions_helper'
 
-require_test_helper_files('../readiness_assurance/helpers/route')
+require_test_helper_files('../readiness_assurance/helpers/route', engine: 'thinkspace-readiness-assurance')
 require_test_helper_files('../readiness_assurance/helpers', engine: 'thinkspace-readiness-assurance')
 Test::Casespace::Seed.load(config: :readiness_assurance)
