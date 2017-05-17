189d6409b7219fad9c17f1d83c11a2e36a1776d5
James Fiderlick <james@sixthedge.com>
2017-05-17 16:29:07 -0700

    [TEST] Trigger deploy [deploy opentbl/staging/client] [deploy opentbl/staging/api]

--- packages/opentbl/deploy/staging/api.rb
+++ packages/opentbl/deploy/staging/api.rb
@@ -8,20 +8,17 @@ class Travis
         end
 
         def self.install
-          commands = [
-            'echo Installing API to $APP_INSTALL_API_DIR',
-            'cd $APP_LOCAL_DIR',
-            '$APP_LOCAL_DIR/install.sh --package opentbl/api --install $APP_INSTALL_API_DIR --platform thinkspace',
-            'mkdir -p $APP_INSTALL_API_DIR/vendor/src/thinkspace/api',
-            'mkdir -p $APP_INSTALL_API_DIR/vendor/src/totem/api',
-            'mkdir -p $APP_INSTALL_API_DIR/.git',
-            'cp -a $APP_SRC_DIR/thinkspace/api/. $APP_INSTALL_API_DIR/vendor/src/thinkspace/api/',
-            'cp -a $APP_SRC_DIR/totem/api/. $APP_INSTALL_API_DIR/vendor/src/totem/api/',
-            'cd $APP_INSTALL_API_DIR',
-            'echo Bundling with $APP_BUNDLE_SRC',
-            'bundle install'
-          ]
-          commands.each { |c| puts `#{c}` }
+          puts `echo Installing API to $APP_INSTALL_API_DIR`
+          Dir.chdir($APP_LOCAL_DIR)
+          puts `./install.sh --package opentbl/api --install $APP_INSTALL_API_DIR --platform thinkspace`
+          puts `mkdir -p $APP_INSTALL_API_DIR/vendor/src/thinkspace/api`
+          puts `mkdir -p $APP_INSTALL_API_DIR/vendor/src/totem/api`
+          puts `mkdir -p $APP_INSTALL_API_DIR/.git`
+          puts `cp -a $APP_SRC_DIR/thinkspace/api/. $APP_INSTALL_API_DIR/vendor/src/thinkspace/api/`
+          puts `cp -a $APP_SRC_DIR/totem/api/. $APP_INSTALL_API_DIR/vendor/src/totem/api/`
+          Dir.chdir($APP_INSTALL_API_DIR)
+          puts `echo Bundling with $APP_BUNDLE_SRC`
+          puts `bundle install`
         end
 
       end
