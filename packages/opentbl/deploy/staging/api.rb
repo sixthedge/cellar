9185e1301a3edca386b90390b56b3320db8962be
James Fiderlick <james@sixthedge.com>
2017-05-17 16:31:24 -0700

    [TEST] Trigger deploy [deploy opentbl/staging/client] [deploy opentbl/staging/api]

--- packages/opentbl/deploy/staging/api.rb
+++ packages/opentbl/deploy/staging/api.rb
@@ -9,14 +9,14 @@ class Travis
 
         def self.install
           puts `echo Installing API to $APP_INSTALL_API_DIR`
-          Dir.chdir($APP_LOCAL_DIR)
+          Dir.chdir(ENV['APP_LOCAL_DIR'])
           puts `./install.sh --package opentbl/api --install $APP_INSTALL_API_DIR --platform thinkspace`
           puts `mkdir -p $APP_INSTALL_API_DIR/vendor/src/thinkspace/api`
           puts `mkdir -p $APP_INSTALL_API_DIR/vendor/src/totem/api`
           puts `mkdir -p $APP_INSTALL_API_DIR/.git`
           puts `cp -a $APP_SRC_DIR/thinkspace/api/. $APP_INSTALL_API_DIR/vendor/src/thinkspace/api/`
           puts `cp -a $APP_SRC_DIR/totem/api/. $APP_INSTALL_API_DIR/vendor/src/totem/api/`
-          Dir.chdir($APP_INSTALL_API_DIR)
+          Dir.chdir(ENV['APP_INSTALL_API_DIR'])
           puts `echo Bundling with $APP_BUNDLE_SRC`
           puts `bundle install`
         end
