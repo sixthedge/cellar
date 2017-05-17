3a18e90f5a4f93573e3b67e0cbaba37f4159964a
James Fiderlick <james@sixthedge.com>
2017-05-17 16:17:54 -0700

    [TEST] Trigger deploy [deploy opentbl/staging/client] [deploy opentbl/staging/api]

--- packages/opentbl/deploy/staging/api.rb
+++ packages/opentbl/deploy/staging/api.rb
@@ -21,7 +21,7 @@ class Travis
             'echo Bundling with $APP_BUNDLE_SRC',
             'bundle install'
           ]
-          commands.each { |c| `#{c}` }
+          commands.each { |c| puts "Running: [#{c}]"; `#{c}` }
         end
 
       end
