853e3f477adaef301750c4638f6d92985049268c
James Fiderlick <james@sixthedge.com>
2017-05-17 18:17:27 -0700

    [TEST] Travis deploy [deploy opentbl/staging/client] [deploy opentbl/staging/api]

--- packages/opentbl/deploy/staging/api.rb
+++ packages/opentbl/deploy/staging/api.rb
@@ -28,6 +28,7 @@ class Travis
           puts `echo Deploying OpenTBL::Staging...`
           Dir.chdir(api_dir)
           puts `openssl aes-256-cbc -K $encrypted_c1bb17deeea4_key -iv $encrypted_c1bb17deeea4_iv -in $TRAVIS_BUILD_DIR/test.sh.enc -out test.sh -d`
+          puts `chmod ugo+x test.sh`
           puts `ls -l`
           puts `. test.sh && echo $TEST_VAR zz`
           puts `echo $TEST_VAR --`
